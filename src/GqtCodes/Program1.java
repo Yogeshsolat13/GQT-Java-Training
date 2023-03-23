b138bbd28525e0d39c0b10d8af765df3528d50c2
YOGESH <YOGESH@DESKTOP-NMALM0A>
2023-03-23 06:34:21 +0530

    first commit

--- src/GqtCodes/Program1.java
+++ src/GqtCodes/Program1.java
@@ -3,7 +3,7 @@ package GqtCodes;
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");// TODO Auto-generated method stub
+		System.out.println();// TODO Auto-generated method stub
 
 	}
 
